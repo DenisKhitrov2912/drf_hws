36a2e162d6af40807a1e1d24d0de2353361fea49
Khitrov <Hitrov.95@yandex.ru>
2024-05-14 16:02:39 +0300

    fix 3rd homework

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD')
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
