a9631f8a05bfb76d7a5dd2a634005c8c54caafc4
Khitrov <Hitrov.95@yandex.ru>
2024-05-14 16:03:17 +0300

    fix 2nd homework

--- config/settings.py
+++ config/settings.py
@@ -85,10 +85,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD')
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
